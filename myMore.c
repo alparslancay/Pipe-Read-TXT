fd7eb351a0d7465b84bf985299568e63fe8bcec2
Alparslan ÇAY <50846539+alparslancay@users.noreply.github.com>
2020-06-07 16:51:33 +0300

    Update myMore.c

--- myMore.c
+++ myMore.c
@@ -7,20 +7,64 @@
 #define READ_END 0
 #define WRITE_END 1
 
+
+int currentIndex = -1;
+char read_msg[9999];
+
+void read24Lines();
+char getAnswer();
+
 int main(int argc, char *argv[]) 
 { 
-  int fd[2] = {atoi(argv[1]),atoi(argv[2])};
+  int fd[2] = {4,5};
 
-  printf("%d",fd[0]);
-  printf("%d",fd[1]);
-  char read_msg[9999];
+
   /* child process */
-    /* close the unused end of the pipe */
-    close(fd[WRITE_END]);
-    /* read from the pipe */
-    read(fd[READ_END],read_msg,9999);
-    printf("Child proses pipe'dan okudu: %s\n",read_msg);
-    /* close the write end of the pipe */
-    close(fd[READ_END]);
+  /* close the unused end of the pipe */
+  close(fd[WRITE_END]);
+  /* read from the pipe */
+  read(fd[READ_END],read_msg,9999);
+  /* close the write end of the pipe */
+  close(fd[READ_END]);
+
+  char choice =' ';
+  char enterPassing;
+
+  while(1){
+  
+    if(choice == ' '){
+      read24Lines();
+    }
+    if(choice == 'q'){
+      break;
+    }
+
+    choice = getAnswer();
+
+  }
   return 0; 
-} 
+}
+
+void read24Lines(){
+
+  char currentChar;
+  printf("\nOKUMA İŞLEMİ YAPILIYOR \n");
+  sleep(1);
+  int currentReadLines = 0;
+  while(currentReadLines < 24){
+    currentChar = read_msg[currentIndex++];
+    printf("%c",currentChar);
+
+    if(currentChar == '\n')
+      currentReadLines++;
+  }
+  printf("\n");
+}
+
+char getAnswer(){
+  char answer;
+  printf("Devam için boşluk değeri girin, çıkmak için 'q' değeri girin:\n");
+  scanf("%c",&answer);
+
+  return answer;
+}
