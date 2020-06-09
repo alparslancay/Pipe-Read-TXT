0f94cc85e88bdbaa2adad14827edc0ac04144a64
Alparslan ÇAY <50846539+alparslancay@users.noreply.github.com>
2020-06-09 20:05:53 +0300

    Update myMore.c

--- myMore.c
+++ myMore.c
@@ -4,20 +4,22 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-#define READ_END 0
-#define WRITE_END 1
+#define READ_END 0 //Defined READ_END Tunel(fd[]) index
+#define WRITE_END 1// Defined WRITE_END Tunel(fd[]) index
 
 
-int currentIndex = -1;
-char read_msg[9999];
+int currentIndex = -1;//Defined currentIndex for read.
+char read_msg[9999];//Defined message index.
 
 void read24Lines();
 char getAnswer();
+void dispose();
+
+int isOver = 0;
 
 int main(int argc, char *argv[]) 
 { 
-  int fd[2] = {4,5};
-
+  int fd[2] = {4,5}; //Defined using tunel numbers.
 
   /* child process */
   /* close the unused end of the pipe */
@@ -27,44 +29,59 @@ int main(int argc, char *argv[])
   /* close the write end of the pipe */
   close(fd[READ_END]);
 
-  char choice =' ';
-  char enterPassing;
-
-  while(1){
+  char choice =' '; //Choice defined.
   
-    if(choice == ' '){
-      read24Lines();
-    }
-    if(choice == 'q'){
-      break;
-    }
+  read24Lines();//Reading first 24 lines.
+
+  while(!isOver)//While until it is over
+  {
 
     choice = getAnswer();
 
+    if(choice == ' ')
+      read24Lines();
+    
+    if(choice == 'q')
+      break;
   }
+  printf("\nmyMore.c end\n");
   return 0; 
 }
 
 void read24Lines(){
 
   char currentChar;
-  printf("\nOKUMA İŞLEMİ YAPILIYOR \n");
+  printf("\n\n----Text Reading---- \n\n");
   sleep(1);
   int currentReadLines = 0;
+
   while(currentReadLines < 24){
     currentChar = read_msg[currentIndex++];
+
+    if(read_msg[currentIndex+1] == '\0')//Next character empty control
+    {
+      printf("\n\nAll texts read!\n\n");
+      dispose();
+      break;
+    }
     printf("%c",currentChar);
 
-    if(currentChar == '\n')
+    if(currentChar == '\n')//last character in current line control.
       currentReadLines++;
   }
   printf("\n");
 }
 
 char getAnswer(){
+
   char answer;
-  printf("Devam için boşluk değeri girin, çıkmak için 'q' değeri girin:\n");
+  printf("For continue enter ' ' (space) value, for exit enter 'q' value:\n");
   scanf("%c",&answer);
 
   return answer;
 }
+
+void dispose(){//For closing this c file.
+  isOver = 1;
+}
+
